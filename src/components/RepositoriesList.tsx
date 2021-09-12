f6e05166e5d8ffc7c5d1ebe4962702badb0e24de
jovanimal <kylcg@hotmail.com>
2021-09-12 18:00:50 +0800

    Bind action creators and implement useSelector

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useActions } from '../hooks/useActions';
 
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState('');
+  const { searchRepositories } = useActions();
+  const { data, error, loading } = useSelector(
+    (state: any) => state.repositories
+  );
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    searchRepositories(term);
+  };
+
   return (
     <div>
-      <form>
-        <input />
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={e => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
     </div>
