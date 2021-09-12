29beab88d1f4c4041bcec170f206c71a25e7b0ec
jovanimal <kylcg@hotmail.com>
2021-09-12 23:26:26 +0800

    Consume store state and display in a list

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import { useSelector } from 'react-redux';
 import { useActions } from '../hooks/useActions';
+import { useTypedSelector } from '../hooks/useTypedSelected';
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
   const { searchRepositories } = useActions();
-  const { data, error, loading } = useSelector(
-    (state: any) => state.repositories
+  const { data, error, loading } = useTypedSelector(
+    state => state.repositories
   );
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -21,6 +21,9 @@ const RepositoriesList: React.FC = () => {
         <input value={term} onChange={e => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map(name => <div key={name}>{name}</div>)}
     </div>
   );
 };
